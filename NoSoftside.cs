7b0e780257407ee452a1d4ca5dd6f07cd0b51906
Billy Joe <arma3cloud@gmail.com>
2021-10-09 16:28:16 -0700

    Initial Commit

---
+++ NoSoftside.cs
@@ -0,0 +1,37 @@
+﻿// Reference: 0Harmony
+using Harmony;
+using UnityEngine;
+
+namespace Oxide.Plugins
+{
+    [Info("No Softside", "Billy Joe", "1.0.0")]
+    [Description("Removes soft sides on objects.")]
+
+    public class NoSoftside : CovalencePlugin
+    {
+        #region Defines
+        private static HarmonyInstance _harmony;
+        #endregion
+
+        #region Hooks
+        private void Loaded()
+        {
+            if (_harmony == null) _harmony = HarmonyInstance.Create("com.BillyJoe.NoSoftside");
+            _harmony.Patch(AccessTools.Method(typeof(DirectionProperties), "IsWeakspot"), new HarmonyMethod(typeof(WeakspotOveride), "Prefix"));
+        }
+
+        private void Unload() => _harmony.UnpatchAll("com.BillyJoe.NoSoftside");
+        #endregion
+
+        #region Patches
+        private static class WeakspotOveride
+        {
+            internal static bool Prefix(ref bool __result)
+            {
+                __result = false;
+                return false;
+            }
+        }
+        #endregion
+    }
+}
